a528858e9f5bfe6fac955e9513b441b7973e5b7a
christopherdeck <cdeck20@optonline.net>
2018-08-09 11:46:26 -0400

    adding loading screen

--- app/src/main/java/tc2/addme/com/addme/AuthenticatorActivity.java
+++ app/src/main/java/tc2/addme/com/addme/AuthenticatorActivity.java
@@ -27,27 +27,35 @@ public class AuthenticatorActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authenticator);
 
-
+        // Add a call to initialize AWSMobileClient
         AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
             @Override
             public void onComplete(AWSStartupResult awsStartupResult) {
-//                SignInUI signin = (SignInUI) AWSMobileClient.getInstance().getClient(AuthenticatorActivity.this, SignInUI.class);
-//                signin.login(AuthenticatorActivity.this, MainActivity.class).execute();
-                AuthUIConfiguration config =
-                    new AuthUIConfiguration.Builder()
-                            .userPools(true)  // true? show the Email and Password UI
-                            .signInButton(FacebookButton.class) // Show Facebook button
-                            .signInButton(GoogleButton.class) // Show Google button
-    //                        .logoResId(R.drawable.mylogo) // Change the logo
-                            .backgroundColor(Color.GRAY) // Change the backgroundColor
-//                            .isBackgroundColorFullScreen(true) // Full screen backgroundColor the backgroundColor full screenff
-                            .fontFamily("sans-serif-light") // Apply sans-serif-light as the global font
-                            .canCancel(true)
-                            .build();
-                SignInUI signinUI = (SignInUI) AWSMobileClient.getInstance().getClient(AuthenticatorActivity.this, SignInUI.class);
-                signinUI.login(AuthenticatorActivity.this, MainActivity.class).authUIConfiguration(config).execute();
+                SignInUI signin = (SignInUI) AWSMobileClient.getInstance().getClient(AuthenticatorActivity.this, SignInUI.class);
+                signin.login(AuthenticatorActivity.this, MainActivity.class).execute();
             }
         }).execute();
+
+//        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+//            @Override
+//            public void onComplete(AWSStartupResult awsStartupResult) {
+////                SignInUI signin = (SignInUI) AWSMobileClient.getInstance().getClient(AuthenticatorActivity.this, SignInUI.class);
+////                signin.login(AuthenticatorActivity.this, MainActivity.class).execute();
+//                AuthUIConfiguration config =
+//                    new AuthUIConfiguration.Builder()
+//                            .userPools(true)  // true? show the Email and Password UI
+//                            .signInButton(FacebookButton.class) // Show Facebook button
+//                            .signInButton(GoogleButton.class) // Show Google button
+//    //                        .logoResId(R.drawable.mylogo) // Change the logo
+//                            .backgroundColor(Color.GRAY) // Change the backgroundColor
+////                            .isBackgroundColorFullScreen(true) // Full screen backgroundColor the backgroundColor full screenff
+//                            .fontFamily("sans-serif-light") // Apply sans-serif-light as the global font
+//                            .canCancel(true)
+//                            .build();
+//                SignInUI signinUI = (SignInUI) AWSMobileClient.getInstance().getClient(AuthenticatorActivity.this, SignInUI.class);
+//                signinUI.login(AuthenticatorActivity.this, MainActivity.class).authUIConfiguration(config).execute();
+//            }
+//        }).execute();
 
         // Sign-in listener
         IdentityManager.getDefaultIdentityManager().addSignInStateChangeListener(new SignInStateChangeListener() {
@@ -65,7 +73,7 @@ public class AuthenticatorActivity extends Activity {
             }
         });
 
-        showSignIn();
+        //showSignIn();
 
     }
 

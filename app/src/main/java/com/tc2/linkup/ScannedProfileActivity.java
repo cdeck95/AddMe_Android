0cde324edbf3c75dcd63ef0d8629886e0466806b
Chris Deck <cdeck20@optonline.net>
2018-11-06 16:18:43 -0500

    starting a hard coded flow for import

--- app/src/main/java/com/tc2/linkup/ScannedProfileActivity.java
+++ app/src/main/java/com/tc2/linkup/ScannedProfileActivity.java
@@ -1,12 +1,27 @@
 package com.tc2.linkup;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ScannedProfileActivity extends AppCompatActivity {
+
+    private static final String TAG = ScannedProfileActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +33,91 @@ public class ScannedProfileActivity extends AppCompatActivity {
 
         TextView profileIdTV = findViewById(R.id.profileIDTextViewImport);
         profileIdTV.setText(profileId);
+    }
+
+    private class GetProfile extends AsyncTask<String, Void, Void> {
+        String title;
+        Context mcontext;
+        MaterialDialog dialog;
+        Integer profileId;
+
+
+        public GetProfile(Context c, Integer profileId) {
+            mcontext = c;
+            this.profileId = profileId;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+//            dialog = new MaterialDialog.Builder(mcontext)
+//                    .title("Contacting Server")
+//                    .content("Loading...")
+//                    .progress(true, 0)
+//                    .progressIndeterminateStyle(true)
+//                    .show();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            JSONObject obj;
+            String cognitoId = CredentialsManager.getInstance().getCognitoId();
+            String urlIn = "https://api.tc2pro.com/users/" + cognitoId + "/profiles/"+ profileId;
+
+            Log.d(TAG, "Cognito ID: " + cognitoId);
+
+            URL url = null;
+            try {
+                url = new URL(urlIn);
+                Log.d(TAG, "URL: " + urlIn);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+
+
+                Log.e(TAG, "Response Code: " + urlConnection.getResponseCode());
+                Log.e(TAG, "Response Message: " + urlConnection.getResponseMessage());
+                if (urlConnection.getResponseCode() == 404) {
+                    Log.e(TAG, "No profile Found.");
+                } else {
+                    if (urlConnection.getInputStream() != null) {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                        String line;
+                        StringBuilder builder = new StringBuilder();
+                        while ((line = in.readLine()) != null) {
+                            builder.append(line);
+                        }
+
+                        Log.d(TAG, "response buffer: " + builder.toString());
+
+                        obj = new JSONObject(builder.toString());
+                        JSONObject profileObject = obj.getJSONObject("profile");
+                        Gson gson = new Gson();
+                        profile = gson.fromJson(profileObject.toString(), Profile.class);
+                    } else {
+                        Log.d(TAG, "No input stream");
+                        return null;
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            populateApps();
+            profileNameTV.setText(profile.getName());
+            profileDescriptionTV.setText(profile.getDescription());
+            userFullNameTV.setText("Chris Deck");
+            EditProfileActivity.DownloadImageWithURLTask downloadTask = new EditProfileActivity.DownloadImageWithURLTask(profileImageView);
+            downloadTask.execute(profile.getImageUrl());
+            //  dialog.dismiss();
+        }
     }
 }

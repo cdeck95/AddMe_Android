f9a74ebd7f465c77c96d67d52b367629526cf3b5
Chris Deck <cdeck20@optonline.net>
2018-11-06 21:41:49 -0500

    started trying to do scan/import with emulator and share... not really working

--- app/src/main/java/com/tc2/linkup/PersonalCodeBottomSheetFragment.java
+++ app/src/main/java/com/tc2/linkup/PersonalCodeBottomSheetFragment.java
@@ -1,12 +1,18 @@
 package com.tc2.linkup;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
@@ -19,6 +25,9 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class PersonalCodeBottomSheetFragment extends BottomSheetDialogFragment {
@@ -33,6 +42,8 @@ public class PersonalCodeBottomSheetFragment extends BottomSheetDialogFragment {
     View contentView;
     String cognitoId;
     Integer profileId;
+    // Uri for image path
+    private static Uri imageUri = null;
 
     private BottomSheetBehavior.BottomSheetCallback mBottomSheetBehaviorCallback = new BottomSheetBehavior.BottomSheetCallback() {
 
@@ -101,9 +112,51 @@ public class PersonalCodeBottomSheetFragment extends BottomSheetDialogFragment {
 //                }
 //                share.putExtra(Intent.EXTRA_STREAM, Uri.parse("file:///sdcard/temporary_file.jpg"));
 //                startActivity(Intent.createChooser(share, "Share Image"));
+            // share image
+            Uri bmpUri = getLocalBitmapUri(imageView);
+            if (bmpUri != null) {
+                // Construct a ShareIntent with link to image
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+                shareIntent.setType("image/*");
+                // Launch sharing dialog for image
+                startActivity(Intent.createChooser(shareIntent, "Share Image"));
+            } else {
+                // ...sharing failed, handle error
+            }
         });
 
     }
+
+    // Returns the URI path to the Bitmap displayed in specified ImageView
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            // Use methods on Context to access package-specific directories on external storage.
+            // This way, you don't need to request external read/write permission.
+            // See https://youtu.be/5xVh-7ywKpE?t=25m25s
+            File file =  new File((Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            // **Warning:** This will fail for API >= 24, use a FileProvider as shown below instead.
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
+    }
+
 
 
     @Override
